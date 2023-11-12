46c1ef18364be0ac6b2728759f95fca9bf4ced0b
lightning5x5 <lightning5x5@gmail.com>
2023-11-12 19:19:43 +0900

    💄 update: Hero 周りのレイアウトを変更

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,19 @@ module.exports = {
   content: ['./index.html'],
   theme: {
     extend: {
+      width: {
+        '78': '19.5rem',
+      },
+      height: {
+        '78': '19.5rem',
+      },
+      borderWidth: {
+        '5': '5px',
+        '6': '6px',
+      },
+      fontSize: {
+        '2.5xl': '1.625rem',
+      },
       keyframes: {
         blink: {
           '0%, 100%': { opacity: 0 },
